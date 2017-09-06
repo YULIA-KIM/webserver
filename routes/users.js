2607643b1925482d7c2bf0d8d509555ff9ee5adb
GAIN LEE <gain@GAINui-MacBook-Pro.local>
2017-09-06 22:15:41 +0900

    [api] 불필요한 부분 제거

--- routes/users.js
+++ routes/users.js
@@ -28,40 +28,6 @@ router.post('/loginOk', function(req, res, next) {
     bverifyUser( userId, password );
   });
 
-  //인증 이부분은 다른 페이지를 요청할때 모두 시행되어야 하는부분// 인증을 한뒤 페이지 결과를 보여줘야한다.
-  function check(req, res){
-    // read the token from header or url
-    const token = req.headers['x-access-token'] || req.params.token
-
-    console.log(token);
-
-    // token does not exist
-    if(!token) {
-        return res.status(403).json({
-            success: false,
-            message: 'not logged in'
-        })
-    }
-
-    var decoded = jwt.verify(token, SECRET);
-          console.log(decoded);
-
-        model.User.findOne({
-          where: {
-            userId: decoded.userID //어떻게 받아오는지 확인해야한다
-          }
-        }).then(function(user){
-          console.log(user);
-          if(!user){
-            console.log("권한이 없습니다.");
-          }else {
-            res.status(200).json({
-              success: true
-            })
-          }
-        });
-  }
-
 //회원가입
 router.post('/signUpOk',function(req, res, next) {
   //res.send('this router is working');
